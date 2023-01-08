d420ffab8e3b3f2d4ba6a51d24e1c89c0084a265
Julian Giron <Jgiron7789@gmail.com>
2023-01-07 18:33:50 -0800

    added postBank, updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank")
 const app = express();
-// app.use(morgan('dev'));
 
-app.get("/", (req, res) => res.send("Aloha!"));
+app.use(morgan("dev"));
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Wizard News</title>
+  </head>
+  <body>
+  <ul>
+    ${posts.map(post => `
+    <div>
+      <h2>${post.title}</h2>
+      <h3>${post.id}</h3>
+    </div>`).join("")}
+  </ul>
+  </body>
+  </html>`;
+
+  res.send(html);
+})
+
 
 const PORT = 1337;
 
